1ba2d7bdc4a351815099e88e464ab04cbfc73cf0
bryanmmr <bryanmmr@gmail.com>
2021-11-25 21:34:11 -0500

    add: counter text area

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = { counter: 0 }
+  handleTextarea = (event) => {
+    this.setState({counter : event.target.value.length})
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.handleTextarea}></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
   }
